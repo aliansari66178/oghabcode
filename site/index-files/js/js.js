92b60e0d9a6fc2ca444c0aeb55fbacb6fd2d664e
AliAnsari <119153810+aliansari66178@users.noreply.github.com>
2022-11-27 12:07:31 +0330

    Add files via upload

---
+++ site/index-files/js/js.js
@@ -0,0 +1,33 @@
+function set (url) {
+    document.getElementById("if").setAttribute("src", url);
+}
+document.getElementById("er").onclick = function () {
+    set("index-files/data/er.html")
+}
+document.getElementById("1").onclick = function () {
+    set("index-files/data/1.html")
+}
+document.getElementById("2").onclick = function () {
+    set("index-files/data/2.html")
+}
+document.getElementById("3").onclick = function () {
+    set("index-files/data/3.html")
+}
+document.getElementById("4").onclick = function () {
+    set("index-files/data/4.html")
+}
+document.getElementById("home").onclick = function () {
+    set("index-files/data/home.html")
+}
+document.getElementById("about").onclick = function () {
+    set("index-files/data/about.html")
+}
+document.getElementById("M").onclick = function () {
+    set("index-files/data/M.html")
+}
+document.getElementById("exe").onclick = function () {
+    set("index-files/data/file/exe.html")
+}
+document.getElementById("sou").onclick = function () {
+    set("index-files/data/file/sou.html")
+}
